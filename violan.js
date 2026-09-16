cd1da71e3ed371e9f75fea85f91fc97b84cdd10f
AKbangash804 <abdullahkhanlive89@gmail.com>
2026-09-16 21:13:33 +0500

    Refactor audio file paths in violan.js

--- violan.js
+++ violan.js
@@ -2,132 +2,132 @@ let body = document.getElementsByTagName('body')[0]
 let audio;
 window.addEventListener('keypress', function (element) {
     if (element.key == 'a') {
-        audio = new Audio('sound/1 key01.ogg')
+        audio = new Audio('1 key01.ogg')
         audio.play()
         body.style.backgroundColor='#00ff00'
     }
     else if (element.key == 's') {
-        audio = new Audio('sound/2 key02.ogg')
+        audio = new Audio('2 key02.ogg')
         audio.play()
         body.style.backgroundColor='#E0FFFF'
     }
     else if (element.key == 'd') {
-        audio = new Audio('sound/key03.ogg')
+        audio = new Audio('key03.ogg')
         audio.play()
         body.style.backgroundColor='#E9967A'
     }
     else if (element.key == 'f') {
-        audio = new Audio('sound/key04.ogg')
+        audio = new Audio('key04.ogg')
         audio.play()
         body.style.backgroundColor='#F400A1'
     }
     else if (element.key == 'g') {
-        audio = new Audio('sound/key05.ogg')
+        audio = new Audio('key05.ogg')
         audio.play()
         body.style.backgroundColor='#FFE302'
     }
     else if (element.key == 'h') {
-        audio = new Audio('sound/key06.ogg')
+        audio = new Audio('key06.ogg')
         audio.play()
         body.style.backgroundColor='#6050DC'
     }
     else if (element.key == 'j') {
-        audio = new Audio('sound/key07.ogg')
+        audio = new Audio('key07.ogg')
         audio.play()
         body.style.backgroundColor='#E62020'
     }
     else if (element.key == 'k') {
-        audio = new Audio('sound/key08.ogg')
+        audio = new Audio('key08.ogg')
         audio.play()
         body.style.backgroundColor='#195905'
     }
     else if (element.key == 'l') {
-        audio = new Audio('sound/key09.ogg')
+        audio = new Audio('key09.ogg')
         audio.play()
         body.style.backgroundColor='#B29700'
     }
     else if (element.key == 'q') {
-        audio = new Audio('sound/key10.ogg')
+        audio = new Audio('key10.ogg')
         audio.play()
         body.style.backgroundColor='#E0FFFF'
     }
     else if (element.key == 'w') {
-        audio = new Audio('sound/key11.ogg')
+        audio = new Audio('key11.ogg')
         audio.play()
         body.style.backgroundColor='#E66771'
     }
     else if (element.key == 'e') {
-        audio = new Audio('sound/key12.ogg')
+        audio = new Audio('key12.ogg')
         audio.play()
         body.style.backgroundColor='#4D5D53'
     }
     else if (element.key == 'r') {
-        audio = new Audio('sound/key13.ogg')
+        audio = new Audio('key13.ogg')
         audio.play()
         body.style.backgroundColor='#F64A8A'
     }
     else if (element.key == 't') {
-        audio = new Audio('sound/key14.ogg')
+        audio = new Audio('key14.ogg')
         audio.play()
         body.style.backgroundColor='#E48400'
     }
         else if (element.key == 'y') {
-        audio = new Audio('sound/key15.ogg')
+        audio = new Audio('key15.ogg')
         audio.play()
         body.style.backgroundColor='#FFD700'
     }
     else if (element.key == 'u') {
-        audio = new Audio('sound/key16.ogg')
+        audio = new Audio('key16.ogg')
         audio.play()
         body.style.backgroundColor='#676767'
     }
     else if (element.key == 'i') {
-        audio = new Audio('sound/key17.ogg')
+        audio = new Audio('key17.ogg')
         audio.play()
         body.style.backgroundColor='#00FF00'
     }
     else if (element.key == 'o') {
-        audio = new Audio('sound/key18.ogg')
+        audio = new Audio('key18.ogg')
         audio.play()
         body.style.backgroundColor='#1164B4'
     }
     else if (element.key == 'p') {
-        audio = new Audio('sound/key19.ogg')
+        audio = new Audio('key19.ogg')
         audio.play()
         body.style.backgroundColor='#923C01'
     }
     else if (element.key == 'z') {
-        audio = new Audio('sound/key20.ogg')
+        audio = new Audio('key20.ogg')
         audio.play()
         body.style.backgroundColor='#ED2939'
     }
     else if (element.key == 'x') {
-        audio = new Audio('sound/key21.ogg')
+        audio = new Audio('key21.ogg')
         audio.play()
         body.style.backgroundColor='#FFFF66'
     }
     else if (element.key == 'c') {
-        audio = new Audio('sound/key22.ogg')
+        audio = new Audio('key22.ogg')
         audio.play()
         body.style.backgroundColor='#4B3621'
     }
     else if (element.key == 'v') {
-        audio = new Audio('sound/key23.ogg')
+        audio = new Audio('key23.ogg')
         audio.play()
         body.style.backgroundColor='#CCCCFF'
     }
     else if (element.key == 'b') {
-        audio = new Audio('sound/key24.ogg')
+        audio = new Audio('key24.ogg')
         audio.play()
         body.style.backgroundColor='#E88E5A'
     }
     else if (element.key == 'n') {
-        audio = new Audio('sound/AURA RINGTONE ☠️.mp3')
+        audio = new Audio('AURA RINGTONE ☠️.mp3')
         audio.play()
         body.style.backgroundColor='#3C341F'
     }
     else if (element.key == 'm') {
-        audio = new Audio('sound/BEST RINGTONE 🤍🎶.mp3')
+        audio = new Audio('BEST RINGTONE 🤍🎶.mp3')
         audio.play()
         body.style.backgroundColor='#E4000F'
     }
